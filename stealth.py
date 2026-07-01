22f13119c6b99ab255f483827de7e62de6447f16
mrjont21-code <mrjont21@gmail.com>
2026-07-02 06:44:11 +0700

    Add files via upload

--- stealth.py
+++ stealth.py
@@ -49,6 +49,10 @@ FIREFOX_HEADERS = {
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate, br",
     "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
 }
 
 
